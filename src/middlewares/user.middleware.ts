0deb284174c734da103e4d307424fc83549761e5
Martin Bachmann <martinpezbach@gmail.com>
2022-10-25 21:18:05 -0300

    feat: implement user validations

---
+++ src/middlewares/user.middleware.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+
+const userSchema = Joi.object({
+  username: Joi.string().required().min(3).messages({
+    'string.empty': '"username" is required',
+    'string.base': '"username" must be a string',
+    'string.min': '"username" length must be at least 3 characters long',
+  }),
+  classe: Joi.string().required().min(3).messages({
+    'string.empty': '"classe" is required',
+    'string.base': '"classe" must be a string',
+    'string.min': '"classe" length must be at least 3 characters long',
+  }),
+  level: Joi.number().required().min(1).messages({
+    'number.empty': '"level" is required',
+    'number.base': '"level" must be a number',
+    'number.min': '"level" must be greater than or equal to 1',
+  }),
+  password: Joi.string().required().min(8).messages({
+    'string.empty': '"password" is required',
+    'string.base': '"password" must be a string',
+    'string.min': '"password" length must be at least 8 characters long',
+  }),
+});
+
+const userMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const validation = userSchema.validate(req.body);
+  if (validation.error) {
+    if (validation.error.details[0].message.match(/required/)) {
+      return res.status(400).json({ message: validation.error.details[0].message });
+    } 
+    return res.status(422).json({ message: validation.error.details[0].message });
+  }
+  next();
+};
+
+export default userMiddleware;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import UserController from '../controller/user.controller';
+import userMiddleware from '../middlewares/user.middleware';
 
 const router = express.Router();
 
 const userController = new UserController();
 
-router.post('/', userController.createUser);
+router.post('/', userMiddleware, userController.createUser);
 
 export default router;
